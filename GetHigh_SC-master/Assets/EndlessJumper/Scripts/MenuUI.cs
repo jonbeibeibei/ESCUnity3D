2701e0c2f03329410097768d3cf19e972e6e9560
Jonathan Bei Qi Yang <jonathanbeiqiyang@Jonathans-MacBook-Pro.local>
2017-03-15 22:40:03 +0800

    First commit

---
+++ GetHigh_SC-master/Assets/EndlessJumper/Scripts/MenuUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour {
+
+	public Canvas MainCanvas; 
+	public Canvas LeaderboardCanvas;
+
+	public SceneManager scene;
+
+	void Awake(){
+		LeaderboardCanvas.enabled = false;
+
+	}
+
+	public void LeaderboardOn(){
+		MainCanvas.enabled = false;
+		LeaderboardCanvas.enabled = true;
+	}
+
+	public void returnFromLeaderboard(){
+		LeaderboardCanvas.enabled = false;
+		MainCanvas.enabled = true;
+	}
+
+
+	public void startSoloPlay(){
+		SceneManager.LoadScene ("Main");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# ESCUnity3D
